67c4ea722a556aa48211384a7957d7814f8c2978
ANDREEA BOBOC <andreea1988iusti2015@gmail.com>
2022-08-27 07:33:47 +0300

    Java Project

--- src/Main7.java
+++ src/Main7.java
@@ -11,6 +11,30 @@ int x = read.getInt();
         System.out.println(x);
 
 
+//2. In clasa Read, duplicati metoda de la punctul 1, astfel incat sa aveti mai multe metode,
+// care sa citeasca si sa returneze si alte tipuri de numere (float, int, double, long, etc)
+
+
+        Read read1 = new Read();
+       float y = read1.getFloat();
+        System.out.println(y);
+
+
+        Read read2 = new Read();
+        double z= read2.getDouble();
+        System.out.println(z);
+
+        Read read3 = new Read();
+        double q= read3.getLong();
+        System.out.println(q);
+
+
+  //3. In clasa Read, creati o metoda care sa returneze un array de numere populat de la tastatura.
+        // Metoda sa primeasca ca si parametru un numar, care reprezinta cate pozitii are.
+
+    Read read4 = new Read();
+    read4.getMyArrayOfInt(args.length);
+
 
     }
 }
--- src/Read.java
+++ src/Read.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,14 +15,109 @@ public class Read {
        do {
            try {
 
-               System.out.println("Please enter a number: ");
+               System.out.println("1. Please enter a integer number: ");
               number = scan.nextInt();
                repeat=false;
            } catch (InputMismatchException exception) {
-               System.err.println("Wrong! This is not a number! " + scan.next());
+               System.err.println("Wrong! This is not a integer number! " + scan.next());
            repeat=true;
            }
        }while (repeat);
    return number;
     }
+
+//2. In clasa Read, duplicati metoda de la punctul 1, astfel incat sa aveti mai multe metode,
+// care sa citeasca si sa returneze si alte tipuri de numere (float, int, double, long, etc)
+
+    public float getFloat(){
+       float number = 0;
+        boolean repeat;
+        Scanner scan = new Scanner(System.in);
+        do {
+            try {
+
+                System.out.println("2. Please enter a float number: ");
+                number = scan.nextFloat();
+                repeat=false;
+            } catch (InputMismatchException exception) {
+                System.err.println("Wrong! This is not a float number! " + scan.next());
+                repeat=true;
+            }
+        }while (repeat);
+        return number;
+    }
+
+
+    public double getDouble(){
+        double number = 0;
+        boolean repeat;
+        Scanner scan = new Scanner(System.in);
+        do {
+            try {
+
+                System.out.println("3. Please enter a double number: ");
+                number = scan.nextDouble();
+                repeat=false;
+            } catch (InputMismatchException exception) {
+                System.err.println("Wrong! This is not a double number! " + scan.next());
+                repeat=true;
+            }
+        }while (repeat);
+        return number;
+    }
+
+
+    public long getLong(){
+        long number = 0;
+        boolean repeat;
+      Scanner scan = new Scanner(System.in);
+        do {
+            try {
+                System.out.println("4. Please enter a long number: ");
+                number = scan.nextLong();
+                repeat=false;
+            } catch (InputMismatchException exception) {
+                System.err.println("Wrong! This is not a long number! " + scan.next());
+                repeat=true;
+            }
+        }while (repeat);
+        return number;
+    }
+
+
+
+ //3. In clasa Read, creati o metoda care sa returneze un array de numere populat de la tastatura.
+    // Metoda sa primeasca ca si parametru un numar, care reprezinta cate pozitii are.
+
+public int[] getMyArrayOfInt(int length){
+    int [] myArray= new int[length];
+    boolean repeat;
+    Scanner scan = new Scanner(System.in);
+
+        do {
+            try {
+
+
+                System.out.println("5. Enter the length of the array:");
+                 length = scan.nextInt();
+
+                System.out.println(" Please enter  elements of the array: ");
+                myArray = new int[length];
+
+                for(int i=0; i<myArray.length; i++){
+                    myArray[i] = scan.nextInt();
+                }
+                System.out.println(Arrays.toString(myArray));
+                repeat=false;
+            } catch (InputMismatchException exception) {
+                System.err.println("Wrong! This is not a array! " + scan.next());
+               repeat=true;
+            }
+        }while (repeat);
+    return myArray;
+
+    }
+
 }
+
+
